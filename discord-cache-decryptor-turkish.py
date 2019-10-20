869a02e2745b98f596a4d5b59bb6964bf547871b
Mert Kemal Atılgan <35312870+mertatilgan@users.noreply.github.com>
2019-10-20 13:11:50 +0300

    Update discord-cache-decryptor-turkish.py

--- discord-cache-decryptor-turkish.py
+++ discord-cache-decryptor-turkish.py
@@ -1,18 +1,21 @@
 import os
+from colorama import Fore, Back, Style
 
 # Cache Dosyasını bulmak için yapmanız gerekenler:
 # Windows arama yerine %appdata% yazın.
 # Discord dosyasını açın.
 # İçinde bulunan cache dosyasının konumunu kopyalayın
 
-print("\u001b[35;1mDiscord Cache Decrypter")
-print("\u001b[37;1mMert Kemal Atılgan tarafından kodlanmıştır.")
+print(Fore.CYAN + "Discord Cache Decrypter")
+print(Fore.WHITE + "Mert Kemal Atılgan tarafından kodlanmıştır.")
 print("https://github.com/mertatilgan\n")
-path = input("Discord'un Cache klasörünün konumunu girin: ")
+
+path = input("Discord'a ait olan Cache klasörünün konumunu girin: ")
 files = os.listdir(path)
 i = 1
 
 for file in files:
     os.rename(os.path.join(path, file), os.path.join(path, str(i)+'.png'))
     i = i+1
-    print("\u001b[32;1m[!] İşlem başarılı. \u001b[33;1m"+path+"\u001b[32;1m konumunda bulunan dosyalar .png formatına çevrildi.\u001b[37;1m")
+    print(Fore.GREEN + "[!] İşlem başarılı."+path+" konumunda bulunan dosyalar .png formatına çevrildi.")
+    print(Style.RESET_ALL)
